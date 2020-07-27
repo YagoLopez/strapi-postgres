f48786d510a94f335544b0797fd8cdf4833e3863
Yago Lopez <yago.lopez@gmail.com>
2020-07-27 17:21:58 +0200

    Refactors

--- config/database.js
+++ config/database.js
@@ -4,8 +4,11 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        host: `/cloudsql/${env('INSTANCE_CONNECTION_NAME')}`,
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
       },
       options: {
         useNullAsDefault: true,
